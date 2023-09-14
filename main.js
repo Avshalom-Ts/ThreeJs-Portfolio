1733de6e1c7e1618cd84743f0dee1fed3664ea25
Avshalom-Ts <avshalomz.ts@gmail.com>
2023-09-14 23:18:32 +0000

    Render the scene in the browser

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
+import './style.css';
 import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
 
 const scene = new THREE.Scene();
+console.log('🚀 ~ file: main.js:4 ~ scene:', scene);
 
-// console.log(scene);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  innerWidth / innerHeight,
+  0.1,
+  1000
+);
+console.log('🚀 ~ file: main.js:13 ~ camera:', camera);
 
-const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight);
+const renderer = new THREE.WebGLRenderer();
+console.log('🚀 ~ file: main.js:16 ~ renderer:', renderer);
+
+renderer.setSize(innerHeight, innerHeight);
+document.body.appendChild(renderer.domElement);

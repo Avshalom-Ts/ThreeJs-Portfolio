a9f9f8c3d1c2b96c98822cf3ab1795ad5939a867
Avshalom-Ts <avshalomz.ts@gmail.com>
2023-09-15 00:02:31 +0000

    Added plane to the scene

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import './style.css';
 import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
 
 const scene = new THREE.Scene();
-console.log('🚀 ~ file: main.js:4 ~ scene:', scene);
+// console.log('🚀 ~ file: main.js:4 ~ scene:', scene);
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -10,27 +10,36 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-console.log('🚀 ~ file: main.js:13 ~ camera:', camera);
+// console.log('🚀 ~ file: main.js:13 ~ camera:', camera);
 
 const renderer = new THREE.WebGLRenderer();
-console.log('🚀 ~ file: main.js:16 ~ renderer:', renderer);
+// console.log('🚀 ~ file: main.js:16 ~ renderer:', renderer);
 
 renderer.setSize(innerHeight, innerHeight);
 renderer.setPixelRatio(devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-console.log('🚀 ~ file: main.js:22 ~ boxGeometry:', boxGeometry);
+// console.log('🚀 ~ file: main.js:22 ~ boxGeometry:', boxGeometry);
 
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-console.log('🚀 ~ file: main.js:25 ~ material:', material);
+// console.log('🚀 ~ file: main.js:25 ~ material:', material);
 
 const mesh = new THREE.Mesh(boxGeometry, material);
-console.log('🚀 ~ file: main.js:28 ~ mesh:', mesh);
+// console.log('🚀 ~ file: main.js:28 ~ mesh:', mesh);
 
 scene.add(mesh);
 
 camera.position.z = 5;
+
+const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
+// console.log('🚀 ~ file: main.js:36 ~ planeGeometry:', planeGeometry);
+
+const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+
+scene.add(planeMesh);
 
 function animate() {
   requestAnimationFrame(animate);
